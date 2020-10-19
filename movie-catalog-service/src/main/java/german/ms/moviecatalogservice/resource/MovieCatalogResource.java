023b00af3a93872788c92c816326e6868f0b015b
german.rokhlov <german.rokhlov@gmail.com>
2020-10-19 16:12:15 +0300

    Added Circuit Breaker using Hystrix for external calls

--- movie-catalog-service/src/main/java/german/ms/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/german/ms/moviecatalogservice/resource/MovieCatalogResource.java
@@ -21,15 +21,39 @@ import java.util.List;
 @RestController
 public class MovieCatalogResource {
 
-//    @Autowired
-//    private RestTemplate restTemplate;
+
+    public List<CatalogItem> listOfMovies = new ArrayList<CatalogItem>(Arrays.asList(
+            new CatalogItem("german", 16),
+            new CatalogItem("german", 17) ,
+            new CatalogItem("alex",101),
+            new CatalogItem("alex",102)
+
+    ));
 
 @Autowired
  private MovieCatalogService movieCatalog;
 
     @RequestMapping ("/catalog/{userId}")
     public List<CatalogItem> getMoviesAndRatingsById (@PathVariable ("userId") String userId){
-        return movieCatalog.getListById(userId);
+        return getListById(userId);
     }
+
+
+
+    public List<CatalogItem> getListById(String userId){
+        List<CatalogItem> listOfMoviesById = new ArrayList<>(Arrays.asList());
+        for (CatalogItem item: listOfMovies) {
+            if (item.getUserId().equals(userId)) {
+                MovieInfo movie = movieCatalog.getMovieInfobyId(item.getMovieId());
+                Ratings rating = movieCatalog.getRatingById(item.getMovieId());
+                System.out.println(movie);
+                System.out.println(rating);
+                CatalogItem theItem = new CatalogItem(userId, movie.getMovieId(), movie.getName(), movie.getDesc(), rating.getRating());
+                listOfMoviesById.add(theItem);
+            }
+        }
+        return listOfMoviesById;
+    }
+
 
 }
--- movie-catalog-service/src/main/java/german/ms/moviecatalogservice/resource/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/german/ms/moviecatalogservice/resource/MovieCatalogService.java
@@ -1,6 +1,7 @@
 package german.ms.moviecatalogservice.resource;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import german.ms.moviecatalogservice.model.CatalogItem;
 import german.ms.moviecatalogservice.model.MovieInfo;
 import german.ms.moviecatalogservice.model.Ratings;
@@ -18,41 +19,26 @@ public class MovieCatalogService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand (fallbackMethod = "fallbackgetRatingbyId")
+    public Ratings getRatingById(int movieId) {
 
-    public List<CatalogItem> listOfMovies = new ArrayList<CatalogItem>(Arrays.asList(
-            new CatalogItem("german", 16),
-            new CatalogItem("german", 17) ,
-            new CatalogItem("alex",101),
-            new CatalogItem("alex",102)
-
-    ));
-
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
-    public List<CatalogItem> getListById(String userId){
-        List<CatalogItem> listOfMoviesById = new ArrayList<>(Arrays.asList());
-        for (CatalogItem item: listOfMovies) {
-            if (item.getUserId().equals(userId)) {
-                Ratings rating = restTemplate.getForObject("http://rating-info-service/ratings/" + item.getMovieId(), Ratings.class);
-                MovieInfo movie =  restTemplate.getForObject("http://movie-info-service/movies/" + item.getMovieId(), MovieInfo.class);
-                System.out.println(movie);
-                CatalogItem theItem = new CatalogItem(userId, movie.getMovieId(), movie.getName(), movie.getDesc(), rating.getRating());
-                listOfMoviesById.add(theItem);
-            }
-        }
-        return listOfMoviesById;
+        Ratings rating = restTemplate.getForObject("http://rating-info-service/ratings/" + movieId, Ratings.class);
+        return rating;
     }
 
-    public List<CatalogItem> getFallbackCatalog(String userId){
-//        List<CatalogItem> listOfMoviesById = new ArrayList<>(Arrays.asList());
-//        for (CatalogItem item: listOfMovies){
-//            if(item.getUserId() == userId){
-//                item.setName("No Movie");
-//                item.setDesc("No Desc");
-//                item.setRating(0.0);
-//                listOfMoviesById.add(item);
-//            }
-//        }
-        return Arrays.asList(new CatalogItem(userId, 0, "No movie", "No movie", 0));
+    public Ratings fallbackgetRatingbyId(int movieId){
+        return new Ratings(movieId, 0);
     }
 
+    @HystrixCommand (fallbackMethod = "fallbackgetMoviebyId")
+    public MovieInfo getMovieInfobyId(int movieId) {
+
+        MovieInfo movie = restTemplate.getForObject("http://movie-info-service/movies/" + movieId, MovieInfo.class);
+        return movie;
+
+    }
+
+    public MovieInfo fallbackgetMoviebyId (int movieId){
+        return new MovieInfo(movieId, "Not found", "Not found");
+    }
 }
